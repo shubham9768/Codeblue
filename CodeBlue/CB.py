d4a13afdd7d34d6b0f7ce230ce08eee276fea92f
Shubham Gaikwad <35921616+shubham9768@users.noreply.github.com>
2019-01-29 22:35:51 +0530

    Add files via upload

---
+++ CodeBlue/CB.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn import preprocessing, model_selection, neighbors
+import pandas as pd
+
+
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+
+#df = df.reset_index()
+
+X = np.array( df.drop(['class'],1))
+y = np.array( df['class'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
+
+#X = X.as_matrix().astype(np.float)
+#y = y.as_matrix().astype(np.float)
+#np.any(np.isnan(mat))
+#np.all(np.isfinite(mat))
+
+#df = df[df.label=='desired_one']
+
+clf = neighbors.KNeighborsClassifier(n_jobs =-1)
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,1,2,3,2,1],[8,10,10,8,7,10,9,7,1]])
+example_measures = example_measures.reshape(len(example_measures), -1)
+prediction = clf.predict(example_measures)
+print(prediction)
